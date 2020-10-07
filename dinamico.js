38f2c7fe4c9fce6e04914178e6db98a47dbe0eac
car123che <3013216470101@ingenieria.usac.edu.gt>
2020-10-06 22:28:25 -0600

    Crear pestañas dinamicamente, y navegar entre ellas

--- dinamico.js
+++ dinamico.js
@@ -1,6 +1,7 @@
+var numeros = [1];
+var areaSelect = ""
 window.onload = function(){
 
-    var numeros = [1];
     $('#btn').addEventListener("click", function(){
         var nuevaLab = document.createElement("label");
         var nombre = "radio" + String((numeros[numeros.length -1] + 1));
@@ -14,12 +15,15 @@ window.onload = function(){
         newContent.setAttribute("type", "radio");
         newContent.setAttribute("class", "radio");
         newContent.setAttribute("id", nombre);
+        newContent.setAttribute("value", nombre);
+        newContent.setAttribute("onClick", "test();")
         const contenido = document.getElementById("content");
         contenido.appendChild(newContent);
 
         var newDiv = document.createElement("div");
         nombre2 = "tab" + String((numeros[numeros.length - 1]));
-        newDiv.setAttribute("class", nombre2);
+        newDiv.setAttribute("class", "tab");
+        newDiv.setAttribute("id", nombre2);
         var texto = document.createElement("p");
         texto.textContent = "texto prueba pela la verga";
         newDiv.appendChild(texto);
@@ -30,14 +34,34 @@ window.onload = function(){
     function $(selector){
         return document.querySelector(selector);
     }
+}
+
+function $(selector){
+    return document.querySelector(selector);
+}
 
 
-    function test(){
-        numeros.forEach(numero => {
-            var seleccion = "#radio" + String(numero);
-            $(seleccion).click(function(){
-                alert(seleccion);
-            });
-        });
-    }
+
+function test(){
+   var seleccion =  $("input[class='radio']:checked").val();
+   seleccion2 = seleccion.replace("radio", "");
+   seleccion2 = "tab" + seleccion2;
+   seleccion = "#" + seleccion
+   $(seleccion).prop("checked", false);
+
+   if(areaSelect==""){
+        const div = document.getElementById(seleccion2);
+        div.classList.remove('tab');
+        div.classList.add('tabS');
+   }else{
+       const divAnterior = document.getElementById(areaSelect);
+       const divActual = document.getElementById(seleccion2);
+       divAnterior.classList.remove('tabS');
+       divAnterior.classList.add('tab');
+
+       divActual.classList.remove('tab');
+       divActual.classList.add('tabS');
+   }
+   areaSelect = seleccion2;
+
 }
